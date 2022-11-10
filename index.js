ea6301935036df1680ce81c84222253c8932ecb0
aknathweb <aniknwu.cse@gmail.com>
2022-11-10 16:29:07 +0600

    review delete part done

--- index.js
+++ index.js
@@ -75,7 +75,12 @@ async function run() {
             const result = await reviewCollection.insertOne(user);
             res.send(result);
         })
-
+        app.delete('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
 
     } catch (error) {
 
