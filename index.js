1665394b9169dc90b689a48fa665e59197eb2283
aknathweb <aniknwu.cse@gmail.com>
2022-11-10 16:25:09 +0600

    done service and sevice details show part

--- index.js
+++ index.js
@@ -36,6 +36,20 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         })
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
 
     } catch (error) {
 
